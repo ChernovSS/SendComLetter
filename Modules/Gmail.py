f36e24919c99f56d291f1f9e6ac477d2a2714acc
sergiuz <mail4chernov@gmail.com>
2021-02-12 14:53:37 +0200

    Add gmail api
    Add gmail api

---
+++ Modules/Gmail.py
@@ -0,0 +1,60 @@
+import os
+import base64
+import pickle
+from pathlib import Path
+from email.mime.text import MIMEText
+import googleapiclient.discovery
+import google_auth_oauthlib.flow
+import google.auth.transport.requests
+
+class Gmail:
+    def __init__(self):
+        pass
+
+    def retry_credential_request(self, force = False):
+        """ Deletes token.pickle file and re-runs the original request function """
+        print("⚠ Insufficient permission, probably due to changing scopes.")
+        i = input("Type [D] to delete token and retry: ") if force == False else 'd'
+        if i.lower() == "d":
+            os.remove("token.pickle")
+            print("Deleted token.pickle")
+            self()
+
+    def get_google_api_credentials(self, scopes):
+        """ Returns credentials for given Google API scope(s) """
+        credentials = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if Path('token.pickle').is_file():
+            with open('token.pickle', 'rb') as token:
+                credentials = pickle.load(token)
+
+        # If there are no (valid) credentials available, let the user log in.
+        if not credentials or not credentials.valid:
+            if credentials and credentials.expired and credentials.refresh_token:
+                credentials.refresh(google.auth.transport.requests.Request())
+            else:
+                flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file('credentials.json', scopes)
+                credentials = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open('../token.pickle', 'wb') as token:
+                pickle.dump(credentials, token)
+        return credentials
+
+    def send_gmail(self, to, subject, message_text):
+        """Send a simple email using Gmail API"""
+        scopes = ['https://www.googleapis.com/auth/gmail.compose']
+        gmail_api = googleapiclient.discovery.build('gmail', 'v1', credentials=self.get_google_api_credentials(scopes))
+        message = MIMEText(message_text)
+        message['to'] = to
+        message['subject'] = subject
+        raw = base64.urlsafe_b64encode(message.as_bytes()).decode()
+        try:
+            request=gmail_api.users().messages().send(userId = 'me', body = {'raw': raw}).execute()
+            print(request)
+        except googleapiclient.errors.HttpError as E:
+            print(E)
+            return
+        return request
+
--- main.py
+++ main.py
@@ -1,14 +1,64 @@
 import json
 
+from Modules.Gmail import Gmail
+
 
 def read_data():
     with open('test.json') as file_json:
         data = json.load(file_json)
     return data
 
+
 def main():
     input_data = read_data()
-    print(input_data)
+    recipients = input_data['recipients']
+    product_lists = input_data['productLists']
+    sender = input_data['sender']
+
+    for recipient in recipients:
+        print(f'{recipient["id"]} - {recipient["fullName"]} - with email {recipient["email"]}')
+        print('---------------------------------------------')
+        # if product_lists['reciepientId'] == recipient['id']
+        # products = [list(product.values())[0] for product in
+        #             [products['products'] for products in product_lists if products['reciepientId'] == recipient['id']]]
+        # print(products)
+
+        products_name = get_products(product_lists,  recipient)
+        print(products_name)
+        if len(products_name) > 0:
+            body = send_mail(products_name, recipient, sender)
+
+            print(body)
+        print('=======================================')
+        print()
+
+
+def send_mail(products_name, recipient, sender):
+    send_to = recipient['email']
+    send_from = sender['email']
+    subject = f'Commercial Proposal'
+    body_header = f'Dear {recipient["fullName"]}!\r\n' \
+                  f'' \
+                  f'We want to offer you the following product:\r\n\r\n'
+    body_footer = f'\r\n\r\n' \
+                  f'Best regards, \r\n' \
+                  f'{sender["name"]} \r\n' \
+                  f'email: {sender["email"]}'
+    body_product_list = ',\r\n'.join(products_name)
+    body = body_header + body_product_list + body_footer;
+
+    gmail = Gmail()
+    gmail.send_gmail(send_to,subject,body)
+    return body
+
+
+def get_products(product_lists, recipient):
+    result = []
+    for products in product_lists:
+        if products['reciepientId'] == recipient['id']:
+            for product in products['products']:
+                result.append(product['name'])
+    return result
 
 if __name__ == "__main__":
     main()
