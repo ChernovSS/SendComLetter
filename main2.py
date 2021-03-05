f5ecdf3a805d912ac32273a421414c9c39096530
sergiuz <mail4chernov@gmail.com>
2021-03-05 16:52:24 +0200

    reformating and add comments / examples

--- main2.py
+++ main2.py
@@ -1,22 +1,40 @@
+from Modules import Types
 from processed import Processed
 
 
 def main():
-    filename = 'test.csv'
-    proc = Processed(filename=filename)
+    # #send from json
+    # filename = 'test.json'
+    # proc = Processed(filename=filename)
+    #
+    # if proc.has_error:
+    #     print(proc.err_message)
+    #
+    # # send from csv
+    # filename = 'test.csv'
+    # proc = Processed(filename=filename)
+    #
+    # if proc.has_error:
+    #     print(proc.err_message)
 
-    if proc.has_error:
-        print(proc.err_message)
+    # send from data
+    recipients = [
+        Types.Contact(id=1, fullname='Test1', email='test1@test.com'),
+        Types.Contact(id=2, fullname='Test2', email='test2@test.com'),
+        Types.Contact(id=3, fullname='Test3', email='test3@test.com')
+    ]
 
-    # products = []
-    # product = Type.Products("test1")
-    # products.append(product)
-    # product2 = Type.Products("test2")
-    # products.append(product2)
-    # proc2 = Processed(products=products)
-    # print("Products: \r\n")
-    # for product in products:
-    #     print(f'{product[0]}')
+    products = [
+        Types.Products(contactid=1, name='product1'),
+        Types.Products(contactid=1, name='product2'),
+        Types.Products(contactid=1, name='product3'),
+        Types.Products(contactid=1, name='product4'),
+        Types.Products(contactid=2, name='product5'),
+        Types.Products(contactid=3, name='product6'),
+    ]
+
+    sender = Types.Contact(id=1, fullname='Sender Letter', email='sender1@test.com')
+    proc = Processed(recipients=recipients, products=products, sender=sender)
 
 
 if __name__ == "__main__":
--- processed.py
+++ processed.py
@@ -1,17 +1,28 @@
-import json
 import csv
+import json
 from typing import List
 
 import Modules.Types as Type
 from Modules.Gmail import Gmail
 
-
+"""
+Processed class for mailings
+"""
 class Processed:
+
 
     def __init__(self, filename: str = None,
                  sender: Type.Contact = None,
                  products: List[Type.Products] = None,
                  recipients: List[Type.Contact] = None):
+        """
+        Init class by next params
+
+        :param filename: input file (.json/.csv) having inputs data
+        :param sender: Types.Contaсts
+        :param products: list Types.Products
+        :param recipients: list Types.Contacts
+        """
         self.filename = filename
         self.sender = sender
         self.products = products
@@ -21,15 +32,17 @@ class Processed:
 
         if filename is not None:
             self.parse_file(filename)
-        # elif all (sender, products, recipients) is not None:
-        #     self.manual_processed(sender, products, recipients)
-        # else:
-        #     self.raiseError("Not Full Parameters")
-
-        if not self.has_error:
+        elif sender is not None and products is not None and recipients is not None:
             self.send_mails()
+        else:
+            self.raiseError("Not Full Parameters")
 
     def parse_file(self, filename):
+        """
+        determinate method for parsing file
+        :param filename:  str filename
+        :return: raise error for class if file extension not in .json or .csv
+        """
         file_extension = filename.split('.')[-1]
         if file_extension.lower() == 'json':
             self.parse_json(filename)
@@ -38,18 +51,25 @@ class Processed:
         else:
             self.raiseError("undestrand")
 
-    def manual_processed(self, sender, products, recipients):
-        pass
-
     def raiseError(self, param: str = None) -> None:
+        """
+        set error message and error flag is true
+        :param param:
+        """
         self.has_error = True
         self.err_message = param
 
     def parse_json(self, filename):
+        """
+        parsing json file to main parameters recipients, products, sender
+        and if class not have error sending mails
+        :param filename: str
+        """
         data = self.read_json(filename)
 
         if self.has_error:
             return False
+
         try:
             self.recipients = []
             for data_one in data[Type.KEY_RECIPIENTS]:
@@ -73,7 +93,14 @@ class Processed:
         except Exception as e:
             self.raiseError(f"Something wrong: \r\n {e}")
 
+        self.send_mails()
+
     def read_json(self, filename):
+        """
+        reading json files to data[]
+        :param filename: str
+        :return: list data or None if raise error
+        """
         data = None
         try:
             with open(filename) as file_json:
@@ -88,7 +115,10 @@ class Processed:
 
     # noinspection PyBroadException
     def read_csv(self, filename):
-        data = None
+        """
+        reading csv files and sending mails if not error
+        :param filename: str
+        """
         try:
             with open(filename, newline='') as csvfile:
                 reader = csv.reader(csvfile, delimiter=',')
@@ -96,23 +126,35 @@ class Processed:
                 index = 1
                 for row in reader:
                     print(row)
+                    self.recipients = []
+                    self.products = []
+
                     new_recipient = Type.Contact(index, row[0], row[1])
-                    new_product = Type.Products(index, )
+                    self.recipients.append(new_recipient)
+                    products_list = row[2].split(';')
+                    for product in products_list:
+                        new_product = Type.Products(index, product.strip())
+                        self.products.append(new_product)
+                    new_sender = Type.Contact(index, row[3], row[4])
+                    self.sender = new_sender
+                    self.send_mails()
             exit()
         except Exception as e:
             self.raiseError(f"Can't read file - {filename}")
 
-        if not ((Type.KEY_RECIPIENTS or Type.KEY_SENDER or Type.KEY_PRODUCT_LISTS) in data):
-            self.raiseError("Json not have all keys")
-
-        return data
-
     def send_mails(self):
+        """
+        sending all email into loop by in list
+        """
         for recepient in self.recipients:
             self.send_mail(recepient)
 
     # noinspection PyTypeChecker
     def send_mail(self, recepient):
+        """
+        send one email to one recipient
+        :param recepient: Types.Contacts
+        """
         products = [product.name for product in self.products if product.contactid == recepient.id or 0]
         if len(products) <= 0:
             self.raiseError(f"Not Products. Email can't sending to {recepient.email}")
@@ -128,7 +170,6 @@ class Processed:
 
     def parse_csv(self, filename):
         data = self.read_csv(filename)
-
         if self.has_error:
             return False
         try:
